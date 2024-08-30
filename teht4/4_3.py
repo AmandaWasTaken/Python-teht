57f99dde55b390e41020afc8c0b2ba6dff82ca38
AmandaWasTaken <amandasandell02@gmail.com>
2024-08-30 14:35:12 +0300

    lisää tehtäviä module 4

--- teht4/4_3.py
+++ teht4/4_3.py
@@ -1,16 +1,15 @@
+num = input("Anna luku:")
+smallest = largest = num
 
-smallest: int   = 0
-largest: int    = 0
 while True:
+    if num != '':
+        if int(num) <= smallest:
+            smallest = int(num)
+            num = input("Anna luku: ")
+        if int(num) >= largest:
+            largest = int(num)
+            num = input("Anna luku: ")
+        else:
+            break
 
-    num = input("Anna luku: ")
-    if int(num) > largest:
-        largest = int(num)
-    elif int(num) < smallest:
-        smallest = int(num)
-    if num == "":
-        break
 
-
-print(f"Suurin {largest}")
-print(f"Pienin: {smallest}")
--- teht4/4_6.py
+++ teht4/4_6.py
@@ -1,15 +1,22 @@
-from random import random
-import math
+import time
+from random import uniform
+from math import pow
 
-pi: float   = 3.14159
+start_time = time.time()
 num_dots    = input("Anna pisteiden määrä ")
 num_dots    = int(num_dots)
 radius: int = 1
-area: float = pi/4
+hits: int   = 0
 
 for i in range(num_dots):
-    dot_x = random()
-    dot_y = random()
-    if (math.pow(dot_x, 2) + math.pow(dot_y, 2)) < 1:
-        print(f"{dot_x} {dot_y}")
+    dot_x   = uniform(-1 ,1)
+    dot_y   = uniform(-1, 1)
+    if pow(dot_x, 2) + pow(dot_y, 2) < 1:
+        hits += 1
 
+
+print(4*hits/num_dots)
+    # N = num_dots, n = hits
+
+end_time = time.time()
+print(f"Aika: {end_time - start_time}")
