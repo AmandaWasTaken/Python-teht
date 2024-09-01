e58244806b2a0bc1053b3831ec3f6798d0bc6669
AmandaWasTaken <amandasandell02@gmail.com>
2024-09-01 20:29:51 +0300

    4_3 ei toimi eikä tule toimimaan

--- teht4/4_3.py
+++ teht4/4_3.py
@@ -1,15 +1,15 @@
-num = input("Anna luku:")
+num = input("Anna luku")
 smallest = largest = num
-
+smallest = int(smallest)
+largest = int(largest)
 while True:
-    if num != '':
-        if int(num) <= smallest:
-            smallest = int(num)
-            num = input("Anna luku: ")
-        if int(num) >= largest:
-            largest = int(num)
-            num = input("Anna luku: ")
-        else:
-            break
-
-
+    num = int(input("Anna luku:"))
+    if num == '':
+        break
+    elif int(num) > largest:
+        largest = num
+        continue
+    elif int(num) < smallest:
+        smallest = num
+        continue
+# ei toimi en saa toimimaan antaa olla
