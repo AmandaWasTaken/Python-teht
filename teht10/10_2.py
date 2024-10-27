dedf9a71c500458b39a8b4180826c4619b2bf5c8
AmandaWasTaken <amandasandell02@gmail.com>
2024-10-27 15:32:09 +0200

    kymppi

---
+++ teht10/10_2.py
@@ -0,0 +1,15 @@
+import House_class as Hc
+
+def main():
+     house = Hc.House(0, 10, 5)
+     house.use_elevator(5, 5)
+     house.fire_emergency()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
---
+++ teht10/House_class.py
@@ -0,0 +1,21 @@
+import Elevator_class as Ec
+
+class House:
+
+    def __init__(self, bottom:int, top:int, n_elevators:int):
+        self.top    = top
+        self.bottom = bottom
+        self.n_elevators = n_elevators
+        self.elevators = []
+        for i in range(n_elevators):
+            elevator = Ec.Elevator(bottom, top)
+            self.elevators.append(elevator)
+
+    def use_elevator(self, elevator_n, floor: int):
+        elev = self.elevators[elevator_n-1]
+        elev.move_to_floor(floor)
+
+    def fire_emergency(self):
+        print("PALOHÄLYTYS!!!!")
+        for e in self.elevators:
+            e.move_to_floor(0)
